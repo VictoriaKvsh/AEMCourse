d47a1ab91cd39deaadc8b855d8c26a51972bb816
Viktoryia_Kuushynava <kuvshinova.vp@gmail.com>
2021-07-28 11:24:21 +0300

    1

---
+++ core/src/main/java/com/gproject/core/models/ExtendedBreadcrumbModel.java
@@ -0,0 +1,10 @@
+package com.gproject.core.models;
+
+
+import com.adobe.cq.wcm.core.components.models.NavigationItem;
+
+import java.util.Collection;
+
+public interface ExtendedBreadcrumbModel {
+    public Collection<NavigationItem> getItems();
+}
